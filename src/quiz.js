c7bc71874a02c042e14bbc80f2468393569f70b3
KelvinFamilia <kelvinjosefamilia@gmail.com>
2025-01-23 16:20:25 +0000

    Mini project ok by Samuel and Kelvin

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,49 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+    shuffleQuestions() {
+        this.questions = this.questions.sort(() => Math.random() - 0.5);
+    }
+    checkAnswer(answer) {
+        if (answer === this.getQuestion().answer) {
+            this.correctAnswers++;
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+        }
+    }
+    hasEnded() {
 
-    // 4. shuffleQuestions()
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else if (this.currentQuestionIndex == this.questions.length) {
+            return true;
+        }
 
-    // 5. checkAnswer(answer)
+    }
+    filterQuestionsByDifficulty(difficulty) {
+        if (difficulty > 0 && difficulty <= 3) {
+            this.questions = this.questions.filter((question) => question.difficulty === difficulty);
+        }
+        console.log(this.questions)
+    }
+    averageDifficulty() {
 
-    // 6. hasEnded()
+        const averageDifficulty = this.questions.reduce((total, quest) => total + quest.difficulty, 0) / this.questions.length;
+
+        console.log(averageDifficulty)
+        return averageDifficulty;
+
+    }
+
+
 }
